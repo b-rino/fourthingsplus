ea24af52f3cfc057c0263baa04a797563c035e0a
Benjamin Rino <148062776+b-rino@users.noreply.github.com>
2025-03-16 16:10:27 +0100

    done/undo DONE

--- src/main/java/app/controllers/TaskController.java
+++ src/main/java/app/controllers/TaskController.java
@@ -14,14 +14,30 @@ public class TaskController {
 
     public static void addRoutes(Javalin app, ConnectionPool connectionPool) {
         app.post("addtask", ctx -> addtask(ctx, connectionPool));
+        app.post("done", ctx -> done(ctx, true, connectionPool));
+        app.post("undo", ctx -> done(ctx, false, connectionPool));
 
+    }
+
+    private static void done(Context ctx, boolean done, ConnectionPool connectionPool) {
+        User user = ctx.sessionAttribute("currentUser");
+        try {
+            int taskId = Integer.parseInt(ctx.formParam("taskId"));
+            TaskMapper.setDoneTo(done, taskId, connectionPool);
+            List<Task> taskList = TaskMapper.getAllTasksPerUser(user.getUserId(), connectionPool);
+            ctx.attribute("taskList", taskList);
+            ctx.render("task.html");
+        }  catch (DatabaseException | NumberFormatException e) {
+            ctx.attribute("message", e.getMessage());
+            ctx.render("index.html");
+        }
     }
 
     private static void addtask(Context ctx, ConnectionPool connectionPool) {
 
         String taskName = ctx.formParam("taskname"); //Sikr dig at det passer på dit name i task.html!
 
-        if (taskName.length() > 3){
+        if (taskName.length() > 3) {
             try {
                 User user = ctx.sessionAttribute("currentUser");
                 Task newTask = TaskMapper.addTask(user, taskName, connectionPool);
